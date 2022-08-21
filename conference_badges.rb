fe35682362663e2681bdb237643ec82b7faa980d
Moringa School <maryanneanyango98@gmail.com>
2022-08-21 15:02:14 +0300

    commit tree 932155c711cf1edf322cc0950951b730b85cce77

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+  end
+  
+  def batch_badge_creator(names)
+    badges = []
+    names.each{|name| badges << ("Hello, my name is #{name}.")}
+    return badges
+  end
+  
+  def assign_rooms(names)
+    badges = []
+    names.each_with_index {|name,index| badges<<("Hello, #{name}! You'll be assigned to room #{index+1}!")}
+    return badges
+  end
+  
+  def printer(names)
+    batch_badge_creator(names).each {|name| puts name}
+    assign_rooms(names).each {|name| puts name}
+  end
+  printer(["Arel", "Carol"])
